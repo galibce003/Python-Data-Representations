bd0db50f502fa1f529be30e8603b1bc006083854
galibce003 <66032815+galibce003@users.noreply.github.com>
2020-07-07 17:50:56 +0600

    Add files via upload

---
+++ list_and_range_2.py
@@ -0,0 +1,35 @@
+#starts with 0, upto but not including 151
+x = range(151)
+
+lst1 = list(x)
+print(lst1)
+
+
+
+
+#starts with 140, upto but not including 151
+x = range(140, 151)
+
+lst2 = list(x)
+print(lst2)
+
+
+
+
+
+#starts with 100, upto but not including 151
+#And increment by 5
+x = range(100, 151, 5)
+
+lst3 = list(x)
+print(lst3)
+
+
+
+
+#starts with 151, upto but not including 100
+#And decrement by 5
+x = range(151, 100, -5)
+
+lst4 = list(x)
+print(lst4)
