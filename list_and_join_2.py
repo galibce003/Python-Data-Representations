584f6e15200c25c177ec896c15e734f464c9e384
galibce003 <66032815+galibce003@users.noreply.github.com>
2020-07-07 18:13:11 +0600

    Add files via upload

---
+++ list_and_join_2.py
@@ -0,0 +1,21 @@
+s1 = ["a", "b", "c", "d", 'e']
+js1 = "".join(s1)     #join by no space
+print(js1)
+
+
+
+
+
+
+s2 = ["a", "b", "c", "d", 'e']
+js2 = " ".join(s2)    #join by no white-space
+print(js2)
+
+
+
+
+
+s3 = ["a", "b", "c", "d", 'e']
+js3 = ",".join(s3)    #join by no comma
+print(js3)
+print(type(js3))      #join makes a string with the items of list
