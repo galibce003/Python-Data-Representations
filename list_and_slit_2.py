2bdcf266dc0f9d325aac1a87da6f1a8580db4d4f
galibce003 <66032815+galibce003@users.noreply.github.com>
2020-07-07 18:07:38 +0600

    Add files via upload

---
+++ list_and_slit_2.py
@@ -0,0 +1,16 @@
+s1 = "I am tomuk "+\
+    "My name is omuk "+\
+    "I do somuk"
+
+print(s1.split())   #split every words and store them in a list
+                    #splitted by whitespace
+
+
+
+
+
+s2 = "I am, tomuk "+\
+    "My name, is omuk "+\
+    "I do, somuk"
+
+print(s2.split(","))     #splitted by ","
