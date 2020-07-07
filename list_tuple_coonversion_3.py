9eb4c636b9d2c7fe65e6ca2189166ab492b7a131
galibce003 <66032815+galibce003@users.noreply.github.com>
2020-07-07 21:40:52 +0600

    Add files via upload

---
+++ list_tuple_coonversion_3.py
@@ -0,0 +1,11 @@
+l1 = [0, 1, 2, 3, 4, 5]
+t1 = tuple(l1)   #tuple convert the list into a tuple
+print(t1)
+#output - (0, 1, 2, 3, 4, 5)
+
+
+
+
+l2 = list(t1)
+print(l2)
+#output - [0, 1, 2, 3, 4, 5]
